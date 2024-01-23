09e194aa348a5ee75d6d555424982f636d74fd3b
Faasinator <faas.fabian@t-online.de>
2024-01-23 07:57:43 +0100

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Fabian Faas on 23.01.24.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
