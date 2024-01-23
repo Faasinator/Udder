a1e36694c6fd8b9448909d02176c4e4070e8d9d0
Faasinator <faas.fabian@t-online.de>
2024-01-23 08:10:30 +0100

    Das ist noch reinzukriegen

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Halloballotallo")
+            Text("hallo")
         }
         .padding()
     }
